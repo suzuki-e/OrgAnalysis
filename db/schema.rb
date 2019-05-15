fef225ad468bdd8f52d6f6da25b33f3c589cf4df
machisuke <d.backford1231@gmail.com>
2019-05-15 17:44:53 +0900

    Add timestamp column to message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_073352) do
+ActiveRecord::Schema.define(version: 2019_05_15_073518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_073352) do
     t.string "ts", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "timestamp", null: false
     t.index ["channel_user_id"], name: "index_messages_on_channel_user_id"
   end
 
