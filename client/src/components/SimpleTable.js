8c4990e00b3cac6e23edc0510b413b2b653d5947
machisuke <d.backford1231@gmail.com>
2019-05-18 17:44:49 +0900

    Change folder structure

--- client/src/components/SimpleTable.js
+++ client/src/components/SimpleTable.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import {withStyles} from '@material-ui/core/styles/index';
+import Table from '@material-ui/core/Table/index';
+import TableBody from '@material-ui/core/TableBody/index';
+import TableCell from '@material-ui/core/TableCell/index';
+import TableHead from '@material-ui/core/TableHead/index';
+import TableRow from '@material-ui/core/TableRow/index';
+import Paper from '@material-ui/core/Paper/index';
 
 const styles = {
   root: {
