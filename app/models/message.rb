95e12f1eb816d10327215a19b5d16c573fb2d245
DaisukeHarada <harada.daisuke@moneyforward.co.jp>
2019-05-22 19:25:49 +0900

    誤削除したコード修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :channel_user
   delegate :channel, to: :channel_user
+  has_many :reactions
   has_many :emojis, through: :reactions
   counter_culture %i[channel_user channel], column_name: 'messages_count'
 end
