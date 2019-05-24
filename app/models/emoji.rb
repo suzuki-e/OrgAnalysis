335d0e4f6af9deeeafe81c96c055cc49ebe2c86e
DaisukeHarada <harada.daisuke@moneyforward.co.jp>
2019-05-24 15:12:55 +0900

    emoji使用回数をcounter_cultureでcounter_cache

--- app/models/emoji.rb
+++ app/models/emoji.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
 class Emoji < ApplicationRecord
+  has_many :reactions
+  has_many :messages, through: :reactions
 end
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,5 @@
 class Reaction < ApplicationRecord
   belongs_to :emoji
   belongs_to :message
+  counter_culture :emoji, column_name: 'used_count', delta_column: 'count'
 end
