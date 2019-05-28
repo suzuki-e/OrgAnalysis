4cb9816ee09d8cb6244aee56fa740fb2969a968c
DaisukeHarada <harada.daisuke@moneyforward.co.jp>
2019-05-28 11:31:54 +0900

    ユーザー一覧をメッセージ順にソート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order(messages_count: 'DESC')
     render json: @users
   end
 
